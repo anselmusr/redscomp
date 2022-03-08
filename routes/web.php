87c74e582df6707c3d63c354721723bbbf943ddf
anselmusr <anselmusrusdiatmaja@gmail.com>
2022-03-09 01:13:36 +0700

    Delete Category

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
     Route::get('categories','App\Http\Controllers\Admin\CategoryController@index');
     Route::get('add-category','App\Http\Controllers\Admin\CategoryController@add');
     Route::post('insert-category', 'App\Http\Controllers\Admin\CategoryController@insert');
-    Route::get('edit-prod/{id}', 'App\Http\Controllers\Admin\CategoryController@edit');
-    //Route::get('edit-prod/{id}',[CategoryController::class, 'edit']);
+    
+    Route::get('edit-prod/{id}',[CategoryController::class, 'edit']);
     Route::put('update-category/{id}', [CategoryController::class, 'update']);
+    Route::get('delete-category/{id}', [CategoryController::class, 'destroy']);
  });
