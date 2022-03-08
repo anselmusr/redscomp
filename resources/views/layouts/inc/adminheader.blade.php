a3dfa944d30bfa5ccd13017ecd3374e664077e88
anselmusr <anselmusrusdiatmaja@gmail.com>
2022-03-08 07:04:32 +0700

    Fetch Category data

--- resources/views/layouts/inc/adminheader.blade.php
+++ resources/views/layouts/inc/adminheader.blade.php
@@ -16,5 +16,6 @@
   <!-- Material Icons -->
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
   <!-- CSS Files -->
+  <link id="pagestyle" href="./admin/css/custom.css " rel="stylesheet" />
   <link id="pagestyle" href="./admin/css/material-dashboard.css?v=3.0.0" rel="stylesheet" />
 </head>
