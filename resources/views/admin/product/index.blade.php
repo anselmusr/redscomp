7bc6008faafa292cd1b8151499bbf977dad34031
anselmusr <anselmusrusdiatmaja@gmail.com>
2022-03-12 22:11:09 +0700

    Edit / update and delete products

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -29,8 +29,8 @@
                         <img src="{{ asset('assets/uploads/products/' .$item->image) }}" class="cate-image">
                     </td>
                     <td>
-                        <a href="#" class="btn btn-primary btn-sm">Edit</a>
-                        <a href="#"class="btn btn-danger btn-sm">Delete</a>
+                        <a href="{{ url('edit-product/'.$item->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                        <a href="{{ url('delete-product/'.$item->id) }}"class="btn btn-danger btn-sm">Delete</a>
                     </td>
                 </tr>
                 @endforeach
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
+use App\Models\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,4 +40,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
     Route::get('products', [ProductController::class, 'index']);
     Route::get('add-products', [ProductController::class, 'add']);
     Route::post('insert-product', [ProductController::class, 'insert']);
+
+    Route::get('edit-product/{id}', [ProductController::class, 'edit']);
+    Route::put('update-product/{id}', [ProductController::class, 'update']);
+    Route::get('delete-product/{id}', [ProductController::class, 'destroy']);
+
  });
