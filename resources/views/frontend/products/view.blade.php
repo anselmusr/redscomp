41cb32862826719e141536092e7e372e57ca83d6
Anselmus Rusdiatmaja <anselmusrusdiatmaja@gmail.com>
2022-04-03 09:38:10 +0700

    integrate font awesome and google fonts

--- resources/views/frontend/products/view.blade.php
+++ resources/views/frontend/products/view.blade.php
@@ -48,8 +48,8 @@
                         </div>
                         <div class="col-md-10">
                             <br/>
-                            <button type="button" class="btn btn-success me-3 float-start">Add to Wishlist</button>
-                            <button type="button" class="btn btn-primary me-3 float-start">Add to cart</button>
+                            <button type="button" class="btn btn-success me-3 float-start">Add to Wishlist <i class="fa fa-heart"></i> </button>
+                            <button type="button" class="btn btn-primary me-3 float-start">Add to cart <i class="fa fa-shopping-cart"></i> </button>
                         </div>
                     </div>
                 </div>
