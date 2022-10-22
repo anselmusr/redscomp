ecc627203009531921faddac542b546e7c06e346
Anselmus Rusdiatmaja <anselmusrusdiatmaja@gmail.com>
2022-10-23 04:11:46 +0700

    Revert "url route"
    This reverts commit 3765e30032d9b86edb78e39542a8013ec18187b3.

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://redscomp.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
